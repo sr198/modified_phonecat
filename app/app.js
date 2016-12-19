faf531c7fb93d4e3c7d83742d9725b1b94c7dff4
Nepal <nepal@net.plm.eds.com>
2016-12-19 11:38:10 -0500

    Changes to be committed: 	new file:   app.js 	modified:   index.html - Introduced basic angular directive

---
+++ app/app.js
@@ -0,0 +1,16 @@
+//Define the phoneCat module
+var phonecatApp = angular.module( 'phonecatApp', []);
+
+//Define a phonelist controller
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+	$scope.phones = [
+		{
+			name: 'Nexus S',
+			snippet: 'Fast just got faster with Nexus S'
+		},
+		{
+			name: 'Motorola XOOM with Wi-Fi',
+			snippet: 'Hey my phone has Wi-Fi'
+		}
+	];
+});
